2a18a798e8cb0e30ec7fe338be479aa862662380
Pothula <a3796@M-IS-2ADHTD6.local>
2017-12-03 17:45:25 -0600

    added exercise-2

---
+++ Ruby/firstlast.rb
@@ -0,0 +1,7 @@
+#Exercise 2 : Write a program to print your first and last name using 2 variables
+firstname="John"
+lastname="Doe"
+puts "Printing firstname and lastname , value of firstname=#{firstname} and value of lastname=#{lastname}"
+puts "My firstname is : #{firstname}"
+puts "My lastname is : #{lastname}"
+puts "My fullname is : #{firstname}" +" "+"#{lastname}"
