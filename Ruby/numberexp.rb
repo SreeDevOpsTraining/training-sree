89d65679e32a505fd96b05c8235364f971dba8f6
Pothula <a3796@M-IS-2ADHTD6.local>
2017-12-03 17:31:18 -0600

    added exercise-1

---
+++ Ruby/numberexp.rb
@@ -0,0 +1,7 @@
+#Exercise 1. Write a ruby program to accept variables a and b and display addition , multiplication , subtraction in 3 statements. 
+a=1
+b=1
+puts "Performing calculations on variables a & b , value of a=#{a} and value of b=#{b}"
+puts "-- add #{a} + #{b} = #{a + b}"
+puts "-- subtract #{a} - #{b} = #{a - b}"
+puts "-- multiply #{a} * #{b} = #{a * b}"
