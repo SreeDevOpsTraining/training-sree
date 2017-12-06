66a45d3d4e38a58f41876b6d15185196614769f3
Pothula <a3796@M-IS-2ADHTD6.local>
2017-12-05 19:31:28 -0600

    added a program on split string into array

---
+++ Ruby/stringsplit.rb
@@ -0,0 +1,28 @@
+#Splitting strings into arrays
+#puts "My firstname. My lastname. My Fullname.".split(/\./).inspect
+name = "My firstname. My lastname. My Fullname.".split(/\./)
+
+p "#{name[0]+name[1]}"
+p "#{name[1]}"
+
+
+#We split the output (string) based on . into an array
+#and we converted into actual string using inspect
+#p is nothing but puts along with inspect in it.
+
+
+#Output of the program.
+#My firstname
+#My lastname
+#My Fullname
+
+#Use case :
+#Lalitha asks for firstname and lastname
+#Suresh asks for lastname
+#Roshan asks for firstname
+#Swaraj asks for fullname
+
+#split is method which splits strings, ruby has already defined method.
+
+#In ruby escape process is prefixed with backslash "\" , which gives more meaning to ruby
+#if you do not do escape , it may interpret wrongly.
